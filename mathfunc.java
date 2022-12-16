271a5fbc311f5ecf1d4040696420f00e840a2ff2
PRANAV <120186818+pranav-tapdiya@users.noreply.github.com>
2022-12-16 23:09:11 +0530

    Create mathfunc.java

---
+++ mathfunc.java
@@ -0,0 +1,8 @@
+public class math {
+    public static void main(String[] args){
+        System.out.println(Math.max(10,5));
+        System.out.println(Math.min(10,5));
+        //taking a random number
+        System.out.println((int)(Math.random()*100));
+    }
+}
